9c0f123053ee9e817b1913db91c1c0e1f9db385b
Mehmet Seven <mehmetseven0@gmail.com>
2021-10-20 15:17:18 +0300

    some changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
 const server = http.createServer(app);
 
 require('dotenv').config();
@@ -18,12 +21,21 @@ const pubClient = createClient({
 });
 const subClient = pubClient.duplicate();
 
+const logger = expressWinston.logger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+  meta: false, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+});
+
 io.adapter(createAdapter(pubClient, subClient));
 io.listen(3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.render('index');
